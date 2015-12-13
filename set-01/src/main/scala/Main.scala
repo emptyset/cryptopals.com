a9f8a7cae387e87b4764ad9bf77f4c324d326d57
Alan Fay <alan@faysoftware.com>
2015-12-13 11:45:34 -0700

    removed challenge 2 from Main

--- set-01/src/main/scala/Main.scala
+++ set-01/src/main/scala/Main.scala
@@ -5,18 +5,8 @@ package tech.fay.matasano
 
 	object Main extends App 
 	{
-		// challenge 2
-		var a = "1c0111001f010100061a024b53535009181c"
-		var b = "686974207468652062756c6c277320657965"
-		var output = "746865206b696420646f6e277420706c6179"
-
-		var result = Util.xor(a, b)
-		var hex = Util.toHex(result)
-		//println(hex)
-		assert(hex == output)
-
 		// challenge 3
-		a = "1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736"
+		var a = "1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736"
 		var ciphertext = Util.fromHex(a)
 		var scoreMap = Decrypt.evaluateAgainstSingleCharacterKeys(ciphertext)
 		//scoreMap.foreach(e => if (e._1 > 100) println(e._1 + "\t" + e._2))
@@ -45,7 +35,7 @@ package tech.fay.matasano
 		{
 			plaintext = ('\n' + line).getBytes()
 			ciphertext = Encrypt.encryptWithRepeatingKeyXor(key, plaintext)
-			hex = Util.toHex(ciphertext)
+			var hex = Util.toHex(ciphertext)
 			println(hex)
 
 			plaintext = Decrypt.decryptWithRepeatingKeyXor(key, ciphertext)
@@ -61,7 +51,7 @@ package tech.fay.matasano
 		var line1 = "0b3637272a2b2e63622c2e69692a23693a2a3c6324202d623d63343c2a26226324272765272a282b2f20"
 		ciphertext = Util.fromHex(line1)
 		plaintext = Decrypt.decryptWithRepeatingKeyXor(key, ciphertext)
-		hex = Decrypt.toString(plaintext)
+		var hex = Decrypt.toString(plaintext)
 		println(line1)
 		println(hex)
 
